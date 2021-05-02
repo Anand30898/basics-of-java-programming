57a25fe4f03f402acb037c10a68ac9cfca51e884
Anand30898 <78747555+Anand30898@users.noreply.github.com>
2021-05-02 19:15:29 +0530

    Update getting_input_array.java

--- array_programs/getting_input_array.java
+++ array_programs/getting_input_array.java
@@ -1,7 +1,22 @@
 //this program the user gets the input from the user
-class Main{
-  public static void main(String[] args){
-    
+import java.util.*;
+import java.io.*;
+
+
+ class Main{
+  public static void main(String[] args)  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Please enter the length of the array");
+    int n =sc.nextInt();
+    int[] a= new int[n];
+    for(int i=0;i<n;i++) {
+      a[i]=sc.nextInt();
+    }
+    //for displaying the array
+    System.out.print(Arrays.toString(a));
+    //one more method to display the array is as follows
+    for(int i=0;i<n;i++){
+    System.out.print(a[i]);
+    }
   }
-    
 }

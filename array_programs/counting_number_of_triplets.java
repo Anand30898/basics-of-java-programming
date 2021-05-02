b44e0cfe287b0f6893240f6f25a8411ce15f40df
Anand30898 <78747555+Anand30898@users.noreply.github.com>
2021-05-02 18:42:57 +0530

    Create counting_number_of_triplets.java

---
+++ array_programs/counting_number_of_triplets.java
@@ -0,0 +1,16 @@
+//Question :-count the number of triplets such that the sum of the third element is equal to the rest two
+class Main{
+  public static void main(String[] args){
+    int a[]= {1,2,5,3,6};
+    int count=0;
+    int n=a.length;
+    Arrays.sort(a);
+    for(int i=0;i<n;i++) {
+    if(i<n-2)
+    if(a[i]+a[i+1]==a[i+2]) {
+    count++;
+    }
+    }
+    System.out.print(count);
+  }
+}  
